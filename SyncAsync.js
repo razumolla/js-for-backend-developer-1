48838196f11f978bb182bbba70f1a0cd38bdefeb
razumolla <rajuahmed9344@gmail.com>
2022-10-25 10:19:52 +0600

    Synchronous & asynchronous

---
+++ SyncAsync.js
@@ -0,0 +1,22 @@
+// class- 1.1
+
+// const colors = require("colors")
+
+//? START
+
+console.log("Welcome to acc");
+
+// setTimeout(() => console.log("I am exciting to teach"), 2000);
+setTimeout(() => {
+    const user = { id: 1 };
+    setTimeout(() => {
+        const products = [{}, {}, {}, {}]
+        setTimeout(() => {
+            const price = [];
+        }, 1000)
+    }, 500)
+}, 2000);
+
+console.log("Hope you are also excited");
+
+// ei vabe async use na kore promise use kora hoi
